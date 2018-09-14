d1bd6b76856f8c6e1c22e31bf3bee1445c0627be
roy769 <roy@mortgallery.com>
2018-09-14 12:18:55 -0700

    back end initiated

--- pebbles/src/App.js
+++ pebbles/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {emojify} from 'react-emojione';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    selectedFile: null
+  }
+  // fileSelectedHandler = event => {
+  // this.setState{(
+  //
+  //   selectedFile:event.target.files[0]
+  //   })
+  // }
+  // fileUploadHandler = () => {
+  //   const fd = new FormData();
+  //
+  //   axios.post
+  // }
   render() {
     return (
       <div className="App">
@@ -16,14 +31,13 @@ class App extends Component {
 
 
                   <h3 className="for-now right">
-                    Pebbles
+                    Vazquez Pool Plastering or Aqua Finishes
                   </h3>
                 </div>
               </div>
             </div>
 
           </nav>
-        </div>
 
         <form>
           <div className="form-group">
@@ -33,31 +47,37 @@ class App extends Component {
            </label>
            <input type="text" className="form-control" id="inputFullName" placeholder=" Full Name"/>
 
-          </div>
-          <div className="form-group">
-            <label for="exampleInputEmail1">Whats the best email to get back to you at?</label>
-            <input type="email" className="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+            <label for="exampleInputEmail">Whats the best
+              {emojify(':e-mail:')}
+              email to get back to you at?</label>
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
 
+            <input type="email" className="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+
+            <label for="zipcode" alt="zip">What's the zipcode for the desired aqua finishing site {emojify(':droplet:')}
+            </label>
+            <input type="integer" className="form-control" id="inputZipcode" placeholder="Enter Zipcode"/>
+
+            <label for="imageUpload" alt="imageUploads">Please upload images of what needs to be finished</label>
+            <input type="file" onChange={this.fileSelectedHandler}/>
+            <button onClick={this.fileUploadHandler}> Upload </button>
           </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-          </div>
-          <div className="form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
+
+
+
+
+          <div className="dropdown-divider"></div>
+
           <button type="submit" className="btn btn-primary">Submit</button>
 
         </form>
 
-<div className="dropdown-divider"></div>
+</div>
 
 
 
 
-      </div>
+</div>
     );
   }
 }
